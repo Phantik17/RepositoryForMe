c7fabd87ce9845c5c7f2a16b57743f1c611068f9
Phantik17 <68153343+Phantik17@users.noreply.github.com>
2026-02-13 16:11:37 +0300

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -6,7 +6,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
-        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, 1), 8)
         self.assertEqual(add(0, 0), 0)
 
     def test_subtract(self):
@@ -35,4 +35,5 @@ class TestCalculator(unittest.TestCase):
 
 
 if __name__ == '__main__':
+
     unittest.main()
